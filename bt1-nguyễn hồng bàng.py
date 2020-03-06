7e594a3b0a142a7264db20db7e101df22297a189
nguyenhongbang298 <61695661+nguyenhongbang298@users.noreply.github.com>
2020-03-06 20:59:57 +0700

    Add files via upload

---
+++ bt1-nguyễn hồng bàng.py
@@ -0,0 +1,7 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello, world!")
+hello, world!
+>>> print("nguyễn hồng bàng - 18575202070005 - ngành điện tử viễn thông")
+nguyễn hồng bàng - 18575202070005 - ngành điện tử viễn thông
+>>> 
